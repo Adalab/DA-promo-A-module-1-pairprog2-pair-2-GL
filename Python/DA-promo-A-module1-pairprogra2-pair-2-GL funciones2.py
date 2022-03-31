ff5d1923d5c3d2188a2c65763629daf07a3e3337
lcp87 <laurapeinadodata@gmail.com>
2022-03-31 18:16:15 +0200

    31/01

---
+++ Python/DA-promo-A-module1-pairprogra2-pair-2-GL funciones2.py
@@ -0,0 +1,51 @@
+# Vamos a crear una "Calculadora de puntos". 
+# Tenéis que escribir una calculadora que reciba cadenas de caracteres como entrada. Los puntos representarán el número de la ecuación. 
+# Habrá puntos en un lado, un operador, y puntos de nuevo después del oparador. Los puntos y el operador estarán separados por un espacio.
+# Aquí os dejamos los operadores válidos:
+# Suma
+# Sustracción
+# Multiplicación
+# División de enteros
+# Vuestro trabajo
+# Tendréis que devolver un string que contenga puntos, tantos como devuelva la ecuación. Si el resultado es 0, devuelve la cadena vacía.
+#  Cuando se trata de una resta, el primer número siempre será mayor o igual que el segundo.
+
+
+#duda para hacerlo en un string (sintrg+string2
+# # o strin1 , simbolo, string2 )
+def calculadora(str_input):
+    string = [str1 + stri]
+    
+    if "+" in string:
+        str1 + str2
+    elif "-" in string:
+        str1 - str2
+    elif "*" in string:
+        str1 * str2
+    elif "/" in string:
+        str1 / str2
+    elif "//" in string:
+        str1 // str2
+
+    return suma
+
+
+print(calculadora("...+...."))
+print(calculadora("...-...."))
+print(calculadora("...*...."))
+print(calculadora(".../...."))
+print(calculadora("...//...."))
+print(calculadora)
+
+
+#parte que funciona con suma +
+# def calculadora(str1,str2):
+#     suma = str(str1 + str2)
+    
+#     if 
+#     return suma
+
+
+# print(calculadora("..", "....."))
+
+  
