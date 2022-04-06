52d89a4135ccb0da6af41ecc800c2a2d40f85895
Laura Peinado <101501188+lcp87@users.noreply.github.com>
2022-04-06 20:09:13 +0200

    Update Pair programming consultas multiples 4.sql

--- SQL/Pair programming consultas multiples 4.sql
+++ SQL/Pair programming consultas multiples 4.sql
@@ -1,12 +1,10 @@
+-- Pair Programming consultas multiples 4 
 USE northwind ;
 
 -- Ejercicio 1
-/*Extraed información de los productos "Beverages".
-En este caso nuestro jefe nos pode que le devolvamos toda la 
-información necesaria para identificar un tipo de producto. En concreto, 
-tienen especial interés por los productos con categoría "Beverages". 
-Devuelve su el ID del producto, el nombre del producto y su ID de 
-categoría.*/
+/*Extraed información de los productos "Beverages". En este caso nuestro jefe nos pode que le devolvamos toda la 
+información necesaria para identificar un tipo de producto. En concreto, tienen especial interés por los productos con categoría "Beverages". 
+Devuelve su el ID del producto, el nombre del producto y su ID de categoría.*/
 
 SELECT products.product_id, products.product_name, categories.category_id, categories.category_name, categories.description
 FROM products
@@ -16,12 +14,13 @@ WHERE category_name = 'Beverages';
 -- stá hecho pero tenemos dudas de que sea tan simple. Info redundante. 
 
 -- Ejercicio 2
-/*Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país.Suponemos que si se trata de ofrecer un mejor tiempo de entrega a los clientes, 
+/*Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país.Suponemos que si se 
+trata de ofrecer un mejor tiempo de entrega a los clientes, 
 entonces podría dirigirse a estos países para buscar proveedores adicionales.*/
 SELECT customer_id, country
 FROM customers
-WHERE country NOT IN (SELECT country
-					FROM suppliers) ;
+WHERE country NOT IN (	SELECT country
+			FROM suppliers) ;
                     
 -- Ejercicio 3
 /*Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"Extraed una lista de pedidos y sus clientes que pidieron más de 20 
@@ -37,10 +36,15 @@ WHERE customer_id = (SELECT order_id
                                         WHERE order_id = 6));
 										
 
+-- Ejercicio 4
+/*Extraed los 10 productos mas caros. Nos siguen pidiendo más queries correlacionadas. En este caso 
+queremos saber cuáles son los 10 productos más caros. Qué producto es más popular.
 
-/*Extraed los 10 productos mas caros
-Nos siguen pidiendo más queries correlacionadas. En este caso 
-queremos saber cuáles son los 10 productos más caros.
-Qué producto es más popular.
-Extraed cuál es el producto que más ha sido comprado y la cantidad que 
+
+
+
+
+
+-- Ejercicio 5
+/* Extraed cuál es el producto que más ha sido comprado y la cantidad que 
 se compró*/
